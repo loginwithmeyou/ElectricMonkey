5d7b6f7e8fae6c12bfbb11a0d4e17126985270bb
Marker <153552873+loginwithmeyou@users.noreply.github.com>
2024-05-13 14:46:36 +0200

    Add files via upload

---
+++ ElectricMonkey.cs
@@ -0,0 +1,16 @@
+using MelonLoader;
+using BTD_Mod_Helper;
+using ElectricMonkey;
+
+[assembly: MelonInfo(typeof(ElectricMonkey.ElectricMonkey), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace ElectricMonkey;
+
+public class ElectricMonkey : BloonsTD6Mod
+{
+    public override void OnApplicationStart()
+    {
+        ModHelper.Msg<ElectricMonkey>("ElectricMonkey loaded!");
+    }
+}
